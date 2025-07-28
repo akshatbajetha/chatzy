c11cdb4da6780449fad22d8b5dd476113cfb158a
Akshat Bajetha <akshatbajetha@gmail.com>
2025-07-28 22:38:57 +0530

    Update backend endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://chatzy.notekaro.com/api",
+      : "https://chatzy-backend.notekaro.com/api",
   withCredentials: true,
 });
